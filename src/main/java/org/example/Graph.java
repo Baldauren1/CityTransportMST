34ff540d0187af4eee69ed7ca3eb9436d6f3a14c
Baldauren1 <baldaurenzamanov@gmail.com>
2025-10-25 13:03:42 +0500

    Add core MST class Graph and it's configuration

---
+++ src/main/java/org/example/Graph.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.*;
+
+public class Graph {
+
+    private final Map<String, List<Edge>> adjacencyList = new HashMap<>();
+
+    public void addNode(String node) {
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(String from, String to, int weight) {
+        addNode(from);
+        addNode(to);
+
+        adjacencyList.get(from).add(new Edge(from, to, weight));
+        adjacencyList.get(to).add(new Edge(to, from, weight)); // так как граф неориентированный
+    }
+
+    public Set<String> getNodes() {
+        return adjacencyList.keySet();
+    }
+
+    public List<Edge> getEdgesFrom(String node) {
+        return adjacencyList.getOrDefault(node, new ArrayList<>());
+    }
+
+    public List<Edge> getAllEdges() {
+        List<Edge> edges = new ArrayList<>();
+        for (List<Edge> list : adjacencyList.values()) {
+            edges.addAll(list);
+        }
+        return edges;
+    }
+
+    public int getVertexCount() {
+        return adjacencyList.size();
+    }
+
+    public int getEdgeCount() {
+        return getAllEdges().size() / 2; // делим на 2, т.к. граф неориентированный
+    }
+}
