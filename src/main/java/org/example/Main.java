50f186a48e5ebf07cbc790031b4e6f250fe27ee8
Baldauren1 <baldaurenzamanov@gmail.com>
2025-10-26 11:15:46 +0500

    develop Main and testing output file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -33,14 +33,14 @@ public class Main {
             long startPrim = System.currentTimeMillis();
             MSTResult primResult = prim.run(graph);
             long endPrim = System.currentTimeMillis();
-            long primTime = endPrim - startPrim;
+            long primTime = (endPrim - startPrim) * 1000;
 
             // Run Kruskal
             KruskalMST kruskal = new KruskalMST();
             long startKruskal = System.currentTimeMillis();
             MSTResult kruskalResult = kruskal.run(graph);
             long endKruskal = System.currentTimeMillis();
-            long kruskalTime = endKruskal - startKruskal;
+            long kruskalTime = (endKruskal - startKruskal) * 1000;
 
             // Write to result JSON
             Map<String, Object> result = new HashMap<>();
@@ -50,11 +50,21 @@ public class Main {
                     "edges", jsonGraph.edges.size()
             ));
             result.put("prim", Map.of(
+                    "mst_edges", primResult.edges.stream().map(e -> Map.of(
+                            "from", e.getFrom(),
+                            "to", e.getTo(),
+                            "weight", e.getWeight()
+                    )).toList(),
                     "total_cost", primResult.totalCost,
                     "operations_count", primResult.operations,
                     "execution_time_ms", primTime
             ));
             result.put("kruskal", Map.of(
+                    "mst_edges", kruskalResult.edges.stream().map(e -> Map.of(
+                            "from", e.getFrom(),
+                            "to", e.getTo(),
+                            "weight", e.getWeight()
+                    )).toList(),
                     "total_cost", kruskalResult.totalCost,
                     "operations_count", kruskalResult.operations,
                     "execution_time_ms", kruskalTime
